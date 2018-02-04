0b175022e4e4bd14a164c9c2c5d693cd2f017023
Jack Geary <Jack.Geary@ed.ac.uk>
2018-02-04 01:05:52 +0000

    Improvements to architecture. Terminates too early

--- map_generator.py
+++ map_generator.py
@@ -16,53 +16,70 @@ class Graph():
         self.coords.append((0,0))
 
 
+    def addLink(self,link,node_from,node_to,direc):
+        link.attachToNodes(node_from,node_to,direc)
+        self.link_list.append(link)
+        self.num_links += 1
+
+
+    def addNode(self,node):
+        self.num_nodes += 1
+        self.coords.append(node.coordinates)
+        self.node_list.append(node)
+
+
     def buildGraph(self,angle_dict,node,node_limit,link_limit,angle_constr=[]):
         open_direc = [x for x in node.directions if node.directions[x] is None]
+        print("LABEL: {}\tAVAIL: {}".format(node.label,open_direc))
         angle = None
-        while angle is None:
-            direc = random.choice(open_direc)
-            if len(angle_dict[direc])>0 and angle not in angle_constr:
-                angle = angle_dict[direc][0]
-            else:
-                open_direc.remove(direc)
-            if open_direc == []:
-                print("ERROR; Not Possible to Construct Graph")
-                exit(-1)
-    
-        link = Link(angle)
-        fail = False
+        num_links = random.randint(1,min(link_limit-self.num_links,len(open_direc)))
+        print("TRYING TO PRINT {} LINKS".format(num_links))
         node_coords = self.coords[self.node_list.index(node)]
-        new_coords = list(node_coords)
-        for i in range(2): new_coords[i] += link.change[i]
-        if new_coords in self.coords:
-            new_node = self.node_list[self.coords.index(new_coords)]
-            if new_node.directions[REVERSE_DICT[direc]] is None:
-                link.attachToNodes(node,new_node,direc)
+        new_links = []
+        new_angles = []
+        for i in range(num_links):
+            angle,direc = getAngle(open_direc,angle_constr,angle_dict)
+            new_angles.append(int(angle))
+            link = Link(angle)
+            fail = True
+            new_coords = list(node_coords)
+            for i in range(2): new_coords[i] += link.change[i]
+            if new_coords in self.coords:
+                new_node = self.node_list[self.coords.index(new_coords)]
+                if new_node.directions[REVERSE_DICT[direc]] is None:
+                    self.addLink(link,node,new_node,direc)
+                    angle_dict[direc].remove(angle)
+                    open_direc.remove(direc)
+                    new_links.append(link)
+                    fail = False
             else:
-                fail  = True
-        else:
-            if self.num_nodes<node_limit:
-                new_node = Node(new_coords[0],new_coords[1],self.num_nodes)
-                self.num_nodes += 1
-                self.coords.append((new_coords))
-                self.node_list.append(new_node)
-                link.attachToNodes(node,new_node,direc)
-            else:
-                fail = True
-
+                if self.num_nodes<node_limit:
+                    new_node = Node(new_coords[0],new_coords[1],self.num_nodes)
+                    self.addNode(new_node)
+                    self.addLink(link,node,new_node,direc)
+                    new_links.append(link)
+                    angle_dict[direc].remove(angle)
+                    open_direc.remove(direc)
+                    print("Created New Node: {}".format(direc))
+                    fail = False
         if fail:
-            angle_constr.append(angle)
+            print("Tried these angles: {} and failed".format(new_angles))
+        else:
+            print("Succeeded in creating a link: {}".format(direc))
+        #exit(-1)
+        if fail:
+            print("Still in same node")
+            for entry in new_angles: angle_constr.append(entry)
             self.buildGraph(angle_dict,node,node_limit,link_limit,angle_constr)
         else:
-            self.link_list.append(link)
-            self.num_links += 1
-            angle_dict[direc].remove(angle)
-            if self.num_links<link_limit:
-                self.buildGraph(angle_dict,new_node,node_limit,link_limit)
-            else:
-                #This is the end condition for the loop that does not exit out of the
-                # program
-                pass
+            print("Going onto other nodes")
+            for link in new_links:
+                if self.num_links<link_limit:
+                    self.buildGraph(angle_dict,link.to_node,node_limit,link_limit)
+                else:
+                    #This is the end condition for the loop that does not exit out of the
+                    # program
+                    pass
                       
  
     def printStatus(self):
@@ -75,7 +92,7 @@ class Graph():
 class Node():
     def __init__(self,i,j,k):
         self.label = k
-        self.coordinates = [i,j]
+        self.coordinates = (i,j)
         self.directions = {"top":None,"bottom":None,"left":None,"right":None}
 
 
@@ -96,6 +113,29 @@ class Link():
         node_to.directions[REVERSE_DICT[from_direc]] = self
         self.from_node = node_from
         self.to_node = node_to
+
+
+def getAngle(open_direc,angle_constr,angle_dict):
+    pos = 0
+    angle = None
+    angle_list = None
+    open_direc = list(open_direc)
+    print("AVAIL: {}".format(open_direc))
+    print("ANGLES: {}".format(angle_dict))
+    print("EXCLUDING: {}".format(angle_constr))
+    while angle is None:
+        direc = random.choice(open_direc)
+        pos = 0
+        if len(angle_dict[direc])>0:
+            angle_list = angle_dict[direc]
+            while pos<len(angle_list) and angle_list[pos] in angle_constr: pos += 1
+            if pos == len(angle_list): open_direc.remove(direc)
+            else: angle = angle_dict[direc][pos]
+        else: open_direc.remove(direc)
+        if open_direc == []:
+            print("ERROR; Not Possible to Construct Graph")
+            exit(-1)
+    return angle,direc
 
 
 def partitionAngles(angles):
@@ -130,25 +170,41 @@ def probAngleSetter(num_angles,rnge):
         prob_angle_list.append(random.randint(rnge[0],rnge[1]))
     return prob_angle_list
 
+def randomiseAngles(count,num_junctions,ang_list):
+    randomiser = None
+    for i,entry in enumerate(ang_list):
+        randomiser = random.choice([True,False])
+        if count<num_junctions and randomiser:
+            ang_list[i] = (entry+180)%360
+            count += 1
+    return ang_list,count
+
 
 def generateAngles(num_junctions,num_roads):
     num_up = None    
     angles = []
+    count = None
 
     while num_up is None or num_up > 2*num_junctions:
         num_up = random.randint(0,num_roads)
 
-    angles += [x for x in probAngleSetter(num_up,(46,135))]
-    angles += [x for x in probAngleSetter(num_roads-num_up,(135,226))]
+    up_angles = [x for x in probAngleSetter(num_up,(46,135))]
+    left_angles = [x for x in probAngleSetter(num_roads-num_up,(135,226))]
 
-    for i,entry in enumerate(angles):
-        if random.randint(0,1):
-            angles[i] = (entry+180)%360
+    for ang_list in [up_angles,left_angles]:
+        count = 0
+        ang_list,count = randomiseAngles(count,num_junctions,ang_list)
+        while len(ang_list)-count>num_junctions:
+            ang_list,count = randomiseAngles(count,num_junctions,ang_list)
+        if count>num_junctions and len(ang_list)-count>num_junctions:
+                print("That's some pretty backward reasoning bro")
+                exit(-1) 
+        angles += ang_list
     return angles
 
 
-num_junctions = 4
-num_roads = 3
+num_junctions = 6
+num_roads = 7
 
 angles = None
 
@@ -161,7 +217,7 @@ for entry in sorted_angles:
     while len(sorted_angles[entry])>num_junctions:
         temp = sorted_angles[entry][0]
         sorted_angles[entry].remove(temp)
-        sorted_angles[REVERSE_DICT[entry]].add(temp)
+        sorted_angles[REVERSE_DICT[entry]].append(temp)
 
 graph = Graph()
 graph.buildGraph(sorted_angles,graph.node_list[0],num_junctions,num_roads)
