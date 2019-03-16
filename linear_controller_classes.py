98204ac8c960fdeed0f8ab55dcba30eb9a45b515
Jack Geary <Jack.Geary@ed.ac.uk>
2019-03-16 17:10:50 +0000

    Minor changes to ProactiveController Definition

--- linear_controller_classes.py
+++ linear_controller_classes.py
@@ -99,11 +99,11 @@ class DrivingController():
         return full_state
 
 
-    def selectAction(self,in_state,accel_range,angle_range):
-        full_state = self.defineState(in_state)
-        accel,angle_accel = self.controller.selectAction(full_state,accel_range,angle_range)
+    def chooseAction(self,state,accel_range,angle_range):
+        #full_state = self.defineState(in_state)
+        accel,angle_accel = self.controller.selectAction(state,accel_range,angle_range)
 
-        #print("Full State: {}\nAccel:{}\n\n".format(full_state,accel))
+        #print("Action here is: {}".format((accel,angle_accel)))
 
         #HACKY: This needs to be removed. Inserted to facilitate first year review results
         next_vel = self.ego.state["velocity"] + self.ego.timestep*accel
@@ -115,8 +115,17 @@ class DrivingController():
         elif next_vel>self.speed_limit:
             accel = min(accel_range[1],max(accel_range[0],(self.speed_limit-self.ego.state["velocity"])/self.ego.timestep))
 
-        self.log.append([full_state,accel])
+        #print("Action to  DelayedController is: {}".format((accel,angle_accel)))
 
+        return (accel,angle_accel)
+
+
+    def selectAction(self,in_state,accel_range,angle_range):
+        state = self.defineState(in_state)
+        action = self.chooseAction(dict(state),accel_range,angle_range)
+
+        (accel,angle_accel) = action
+        self.log.append([state,accel])
         return accel,angle_accel
 
 
@@ -133,7 +142,7 @@ class DrivingController():
 
 
     def testAction(self,accel,angle_accel,vel=None,heading=None):
-        x_dot,y_dot,v_dot,head_dot = self.ego.simulateDynamics(accel,angel_accel,vel,heading)
+        x_dot,y_dot,v_dot,head_dot = self.ego.simulateDynamics(accel,angle_accel,vel,heading)
         return x_dot,y_dot,v_dot,head_dot
 
 
@@ -145,13 +154,30 @@ class DelayedDrivingController(DrivingController):
 
         self.action_list = [(0,0) for _ in range(int(delay/timestep))]
 
-    def selectAction(self,*args):
-        accel,angle_accel = super().selectAction(*args)
-        self.action_list.append((accel,angle_accel))
-        print("ACTION_LIST: {}".format(self.action_list))
-        (accel,angle_accel) = self.action_list.pop(0)
 
+    def chooseAction(self,*args):
+        action = super().chooseAction(*args)
+        #print("Action received by DelayedController is: {}".format(action))
+        self.action_list.append(action)
+        action = self.action_list.pop(0)
+
+        #print("ActionList for DelayedController is: {}".format(self.action_list))
+        #print("Action output by DelayedController: {}".format(action))
+        return action
+
+
+    def selectAction(self,in_state,*args):
+        state = super().defineState(in_state)
+        action = self.chooseAction(dict(state),*args)
+
+        (accel,angle_accel) = action
+        self.log.append([state,accel])
         return accel,angle_accel
+
+
+    def reset(self):
+        super().reset()
+        self.action_list = [(0,0) for _ in range(len(self.action_list))]
 
 
 class ProactiveDrivingController(DelayedDrivingController):
@@ -159,52 +185,77 @@ class ProactiveDrivingController(DelayedDrivingController):
     def __init__(self,anticipation_time=0,awareness_coef=.1,non_ego_trajectory=None,timestep=.1,**kwargs):
         kwargs.update({'timestep':timestep})
 
-        super().__init__(self,**kwargs)
+        super().__init__(**kwargs)
         self.timestep = timestep
+        self.awareness = awareness_coef
+        self.anticipation_time = int(anticipation_time/timestep)
 
         if non_ego_trajectory is not None:
             self.other_trajectory = non_ego_trajectory
-            self.anticipation_time = anticipation_time
             self.traj_index = 0
-            self.awareness =  awareness_coef
         else:
             self.other_trajectory = None
-            self.anticipation_time = 0
             self.traj_index = None
-            self.awareness = None
+
+
+    def setTargetTrajectory(self,trajectory):
+        self.other_trajectory = trajectory
+        self.traj_index = 0
+
+
+    def chooseAction(self,state,accel_range,angle_range):
+        tot_action = [0,0]
+        determinant = 0
+        #print("ActionList for ProactiveController is: {}".format(self.action_list))
+        action_list = None #temporary copy of list
+        #print("Proactive")
+        for i in range(0,self.anticipation_time+1):
+            action = super().chooseAction(state,accel_range,angle_range)
+            #print("In ProactiveController in Loop ActionList is: {}".format(self.action_list))
+            if i==0: action_list = list(self.action_list) #The main action_list only wants to store what you woul
+            #print("I: {}\t State:{}\tAction: {}".format(i,state,action))
+            tot_action = [x+(self.awareness**i)*y for x,y in zip(tot_action,action)]
+            determinant += (self.awareness**i)
+
+            if self.other_trajectory is None or self.traj_index+i>=len(self.other_trajectory): break
+            else:
+                traj_point = self.other_trajectory[self.traj_index+i]
+                if (self.other_trajectory[self.traj_index][1]<0) != (traj_point[1]<0):# or self.other_trajectory[self.traj_index][1]>0 != traj_point[1]>0:
+                    #print("Proactive Controller Breaking out: {}vs.{}".format(self.other_trajectory[self.traj_index][1],traj_point[1]))
+                    #This cripples the proactive controller so it cannot anticipate actions that are not consistent with the current trend
+                    break
+                #Placeholder value of 90 for heading since we know trajectory will be linear
+                x_dot,y_dot,v_dot,head_dot = super().testAction(traj_point[1],0,state["other_v"],90)
+                state["other_v"] = state["other_v"] + v_dot*self.timestep #we assume trajectory has same length timestep
+                state["other_position"] = (state["other_position"][0]+x_dot*self.timestep,state["other_position"][1]+y_dot*self.timestep)
+                state["other_accel"] = traj_point[1]
+
+                x_dot,y_dot,v_dot,head_dot = super().testAction(action[0],action[1],state["velocity"],state["heading"])
+                state["position"] = (state["position"][0]+x_dot*self.timestep,state["position"][1]+y_dot*self.timestep)
+                state["velocity"] = state["velocity"]+v_dot*self.timestep
+                state["heading"] = state["heading"]+head_dot*self.timestep
+                state = super().inferFeatures(state)
+
+        #print("\n")
+        tot_action = [x/determinant for x in tot_action]
+        self.action_list = action_list
+        #print("Tot Action at end of ProactiveController is: {}".format(tot_action))
+        #print("ActionList at end of ProactiveController is: {} with {} the chosen action".format(self.action_list,tot_action))
+        return (tot_action[0],tot_action[1])
 
 
     def selectAction(self,in_state,accel_range,angle_range):
         state = super().defineState(in_state)
 
-        if self.other_trajectory is not None and state["other_position"] != self.other_trajectory[self.traj_index][0]:
-            print("Error: Controller mismatch\nProactive Controller has NE at: {} and Sub Controller has at: {}".format(self.other_trajectory[self.traj_index][0],state["other_position"]))
-            exit(-1)
+        action = self.chooseAction(dict(state),accel_range,angle_range)
+        (accel,angle_accel) = action
 
-        tot_accel = [0,0]
-        determinant = 0
-        for i in range(0,self.anticipation_time+1):
-            action = self.controller.selectAction(state,accel_range,angle_range)
-            tot_accel = [x+(self.awareness**i)*y for x,y in zip(tot_accel,action)]
-            determinant += (self.awareness**i)
+        self.log.append([state,accel])
+        if self.other_trajectory is not None:
+            self.traj_index += 1
 
-            if self.other_trajectory is None or self.traj_index+i+1>=len(self.other_trajectory): break
-            else:
-                traj_point = self.other_trajectory[self.traj_index+i+1]
-                state["other_v"] = traj_point[1]
-                state["other_position"] = traj_point[0]
-                self["other_accel"] = traj_point[2]
-
-                x_dot,y_dot,v_dot,head_dot = super().testAction(action[0],action[1],state["velocity"],state["heading"])
-                state["position"][0] += x_dot*timestep
-                state["position"][1] += y_dot*timestep
-                state["velocity"] += v_dot*timestep
-                state["heading"] += head_dot*timestep
-                state = super().inferFeatures(state)
-
-        tot_accel = [x/determinant for x in tot_accel]
-        self.traj_index += 1
-        return tot_accel[0],tot_accel[1]
+        #print("Chosen Action is: {}\n".format((accel,angle_accel)))
+        return accel,angle_accel
 
 
     def reset(self):
